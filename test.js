39b01cd8bb98bbc6f962a5f2b4e345a4cfd56ec2
Cristi Constantin <cristi.constantin@live.com>
2018-01-21 18:18:18 +0200

    test: compress and list

--- test.js
+++ test.js
@@ -4,9 +4,31 @@ import test from 'ava'
 
 test('compress and decompress a file', async t => {
   const testFile = 'fixtures/a.txt'
+  const testArch = 'a.tgz'
+
   const initialText = fs.readFileSync(testFile, 'utf8')
-  await tar.tarCompress('a.tgz', testFile)
-  await tar.tarExtract('a.tgz')
+  await tar.tarCompress(testArch, testFile)
+  await tar.tarExtract(testArch)
   const finalText = fs.readFileSync(testFile, 'utf8')
+  fs.unlinkSync(testArch) // cleanup
+
   t.is(initialText, finalText)
+})
+
+test('compress and list a folder', async t => {
+  const testPath = 'fixtures/'
+  const testArch = 'fix.tgz'
+
+  await tar.tarCompress(testArch, testPath)
+  const files = await tar.tarList(testArch)
+  fs.unlinkSync(testArch) // cleanup
+
+  t.deepEqual(files, [
+    'fixtures/',
+    'fixtures/a.txt',
+    'fixtures/deep/',
+    'fixtures/deep/b.txt',
+    'fixtures/deep/deeper/',
+    'fixtures/deep/deeper/c.txt'
+  ])
 })
